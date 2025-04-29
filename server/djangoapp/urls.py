4bed375ef9ff4f568020a252b05e26d7827f295c
vipra04 <119961339+vipra04@users.noreply.github.com>
2025-04-29 19:42:02 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,10 +7,14 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
+    path(route='registration', view=views.registration, name='registration'),
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
-    
+
+    # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
+
     # path for dealer reviews view
 
     # path for add a review view
