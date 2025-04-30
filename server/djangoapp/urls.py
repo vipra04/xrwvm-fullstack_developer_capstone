5c949c93168166d52abb64738989fbacf7972db9
vipra04 <119961339+vipra04@users.noreply.github.com>
2025-04-30 22:53:19 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
 
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
